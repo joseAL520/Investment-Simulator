36ae665b8a61e5b25234f99feff48a9bbb089ae5
jose angel <jose.albelt1817@gmail.com>
2025-04-10 10:24:34 -0500

    siderBarDesign - ROUTERLINK

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import { Sidebar } from './components/ui/Sidebar'
+import { Dashboard } from './pages/Dashboard'
+import { Historial } from './pages/Historial'
+import { Portafolio } from './pages/Portafolio'
 
 function App() {
 
   return (
-    <> 
-      <Sidebar></Sidebar>
-    </>
+    <Router>
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-1 p-6">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/history" element={<Historial />} />
+          <Route path="/portfoli" element={<Portafolio />} />
+        </Routes>
+      </main>
+    </div>
+  </Router>
   )
 }
 
