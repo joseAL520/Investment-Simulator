051a1fabd6651194ad0d73e4516844bf4c90d36f
jose angel <jose.albelt1817@gmail.com>
2025-04-10 10:51:51 -0500

    struture dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="grid grid-cols-3 grid-rows-5 gap-4">
+        <div className="col-span-3 bg-base-200">
+          1 - Cards
+        </div>
+        
+        <div className="col-span-2 row-span-2 row-start-2  bg-base-200 rounded-md p-3" >
+          2 - grafica 
+        </div>
+        
+        <div className="row-span-3 col-start-3 row-start-2 bg-base-200 rounded-md p-3" >
+          3 - compra y venta de activos
+        </div>
+
+        <div className="col-span-2 row-span-2 row-start-4 bg-base-200 rounded-md p-3" >
+          4 - tabla de activos recientes
+        </div>
+    </div>
+        
   )
 }
