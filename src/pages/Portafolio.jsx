41c24d9f03841d5c4e417d269cc13c2475d8caec
jose angel <jose.albelt1817@gmail.com>
2025-04-12 11:54:45 -0500

    pieGrafic

--- src/pages/Portafolio.jsx
+++ src/pages/Portafolio.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Card from '../components/ui/Card'
+import PieChart from '../components/charts/PieGrafic'
 
 export const Portafolio = () => {
   return (
-    <div className='flex flex-col m-5 p-2 rounded-md gap-3 '> 
+    <div className='flex flex-col m-5 p-2 rounded-md gap-3'> 
       
       <section><Card></Card></section>
-      <section>2</section>
+      <div className="grid place-items-center">
+        <section className='bg-base-200 rounded-md p-4 w-150 ' > <PieChart></PieChart> </section>
+      </div>
+
       <section>3</section>
 
       
