a92488afae5d6d9029eced70ac9d1c7ebde0b7ce
jose angel <jose.albelt1817@gmail.com>
2025-04-12 10:29:28 -0500

    Modal of infoTran

--- src/components/tableHistoryCripto/TableHistoryCrypto.jsx
+++ src/components/tableHistoryCripto/TableHistoryCrypto.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import  {Modal}  from '../ui/Modal'
 
 // pedir un limite de 3 en el dashboard
 // en el historial mosrar normal limite de 10 por paginacion
 // metodo LIFO
 
-
 export const TableHistoryCrypto = () => {
-  return (
+  return (<>
+
 <div className="overflow-x-auto flex flex-col items-center">
   <table className="table">
     <thead>
@@ -21,6 +22,7 @@ export const TableHistoryCrypto = () => {
       </tr>
     </thead>
     <tbody>
+
       <tr>
         <th>
             <div >
@@ -48,71 +50,11 @@ export const TableHistoryCrypto = () => {
         <td>$ 2.5000</td>
         <td className='text-error'>-15</td>
         <td className="text-sm opacity-50">
-         <button className='btn btn-ghost'>Detalle</button>
+          {/* enviar datos */}
+          {<Modal data={['Bitcoin','Compra','2.5 BTC',1.850,'01/02/2025 14:35','0XABC1234..FREE',12,4.5,'TXN-01231afdfasd']}/>}
         </td>
       </tr>
 
-      <tr>
-        <th>
-            <div >
-                <div className="font-bold">2/5/2025</div>
-                <div className="text-sm opacity-50">1:30 PM</div>
-            </div>
-        </th>
-        <td>
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle h-12 w-12">
-                <img
-                  src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                  alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">BitCoin</div>
-              <div className="text-sm opacity-50">BTC</div>
-            </div>
-          </div>
-        </td>
-        <td className='text-success'>Venta</td>
-        <td>2.5</td>
-        <td>$ 2.5000</td>
-        <td className='text-success'>+15</td>
-        <td className="text-sm opacity-50">
-         <button className='btn btn-ghost'>Detalle</button>
-        </td>
-      </tr>
-
-      <tr>
-        <th>
-            <div >
-                <div className="font-bold">2/5/2025</div>
-                <div className="text-sm opacity-50">1:30 PM</div>
-            </div>
-        </th>
-        <td>
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle h-12 w-12">
-                <img
-                  src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                  alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">BitCoin</div>
-              <div className="text-sm opacity-50">BTC</div>
-            </div>
-          </div>
-        </td>
-        <td className='text-success'>Venta</td>
-        <td>2.5</td>
-        <td>$ 2.5000</td>
-        <td className='text-success'>+15</td>
-        <td className="text-sm opacity-50">
-         <button className='btn btn-ghost'>Detalle</button>
-        </td>
-      </tr>
 
     </tbody>
   </table>
@@ -122,5 +64,7 @@ export const TableHistoryCrypto = () => {
         <button className="join-item btn">»</button>
     </div>
 </div>
-  )
+  
+  
+  </>)
 }
---
+++ src/components/ui/Modal.jsx
@@ -0,0 +1,62 @@
+export const Modal = ({data}) => {
+ 
+return (<>
+
+  <button className="btn" onClick={()=>document.getElementById('my_modal_2').showModal()}>Mas</button>
+  
+  <dialog id="my_modal_2" className="modal">
+    <div className="modal-box">
+
+      <h3 className="font-bold text-lg">Detalle de la Transacción</h3>
+
+      <div className="grid grid-cols-2 grid-rows-9 gap-3 m-3 p-3">
+        <div ><label className="label">Activo</label></div>
+        <div className="col-start-1 row-start-2">  <label className="label"> Tipo de Operación  </label>  </div>
+        <div className="col-start-1 row-start-3">  <label className="label"> Cantidad  </label>  </div>
+        <div className="col-start-1 row-start-4">  <label className="label"> Valor Total  </label>  </div>
+        <div className="col-start-1 row-start-5">  <label className="label"> Fecha y Hora  </label>  </div>
+        <div className="col-start-1 row-start-6">  <label className="label"> Wallet Usada  </label>  </div>
+        <div className="col-start-1 row-start-7">  <label className="label"> Confirmaciones  </label>  </div>
+        <div className="col-start-1 row-start-8">  <label className="label"> ROI actual  </label>  </div>
+        <div className="col-start-1 row-start-9">  <label className="label"> ID de transacción  </label>  </div>
+              {/* data */}
+
+        <div className="col-start-2 row-start-1 font-bold"> 
+          <div className="flex gap-0.5">
+            <img src="https://cdn-icons-png.flaticon.com/512/5968/5968260.png" alt="ll" width={20} height={20} /> {data[0]}
+          </div>
+        </div>
+        <div className="col-start-2 row-start-2 font-bold">{data[1]}</div>
+        <div className="col-start-2 row-start-3 font-bold">{data[2]}</div>
+        <div className="col-start-2 row-start-4 font-bold">$ {data[3]}</div>
+        <div className="col-start-2 row-start-5 font-bold">{data[4]}</div>
+        <div className="col-start-2 row-start-6 font-bold">{data[5]}</div>
+        <div className="col-start-2 row-start-7 font-bold">{data[6]}</div>
+        <div className="col-start-2 row-start-8 font-bold">
+          { data[7] < 0 ? <label className="text-error">{data[7]}%</label> : <label className="text-success">+{data[7]}%</label>}
+        </div>
+        <div className="col-start-2 row-start-9 font-bold">{data[8]}</div>
+      </div>
+      
+      <div className="flex justify-end">
+          <button className="btn  ">
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              width={15} 
+              height={15} 
+              viewBox="0 0 15 15">
+              <path fill="currentColor" d="M2.5 6.5V6H2v.5zm4 0V6H6v.5zm0 4H6v.5h.5zm7-7h.5v-.207l-.146-.147zm-3-3l.354-.354L10.707 0H10.5zM2.5 7h1V6h-1zm.5 4V8.5H2V11zm0-2.5v-2H2v2zm.5-.5h-1v1h1zm.5-.5a.5.5 0 0 1-.5.5v1A1.5 1.5 0 0 0 5 7.5zM3.5 7a.5.5 0 0 1 .5.5h1A1.5 1.5 0 0 0 3.5 6zM6 6.5v4h1v-4zm.5 4.5h1v-1h-1zM9 9.5v-2H8v2zM7.5 6h-1v1h1zM9 7.5A1.5 1.5 0 0 0 7.5 6v1a.5.5 0 0 1 .5.5zM7.5 11A1.5 1.5 0 0 0 9 9.5H8a.5.5 0 0 1-.5.5zM10 6v5h1V6zm.5 1H13V6h-2.5zm0 2H12V8h-1.5zM2 5V1.5H1V5zm11-1.5V5h1V3.5zM2.5 1h8V0h-8zm7.646-.146l3 3l.708-.708l-3-3zM2 1.5a.5.5 0 0 1 .5-.5V0A1.5 1.5 0 0 0 1 1.5zM1 12v1.5h1V12zm1.5 3h10v-1h-10zM14 13.5V12h-1v1.5zM12.5 15a1.5 1.5 0 0 0 1.5-1.5h-1a.5.5 0 0 1-.5.5zM1 13.5A1.5 1.5 0 0 0 2.5 15v-1a.5.5 0 0 1-.5-.5z"></path>
+            </svg>
+            Exportar PDF
+          </button>
+      </div>
+      
+    </div>
+
+    <form method="dialog" className="modal-backdrop">
+      <button>close</button>
+    </form>
+
+  </dialog>
+  
+</>)}
